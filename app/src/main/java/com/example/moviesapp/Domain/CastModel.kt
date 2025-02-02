c0ed40038973e8a063df4aa958dfc8b38fefb537
saeed <saeed60914376@gmail.com>
2025-02-02 13:08:39 +0330

    Main Repository

--- app/src/main/java/com/example/moviesapp/Domain/CastModel.kt
+++ app/src/main/java/com/example/moviesapp/Domain/CastModel.kt
@@ -1,6 +1,10 @@
 package com.example.moviesapp.Domain
 
+import java.io.Serializable
+
 data class CastModel(
     var PicUrl: String = "",
     var Actor: String = ""
-)
+): Serializable
+// Serializable used to convert objects into a byte stream,
+// allowing them to be saved to a file, sent over a network, or passed between Android components.
---
+++ app/src/main/java/com/example/moviesapp/Repository/MainRepository.kt
@@ -0,0 +1,58 @@
+package com.example.moviesapp.Repository
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.example.moviesapp.Domain.FilmItemModel
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+
+class MainRepository {
+    private val firebaseDatabase = FirebaseDatabase.getInstance()
+
+    //LiveData: A lifecycle-aware data holder that allows UI components (like ViewModel) to observe changes and update automatically.
+    //MutableLiveData: A mutable version of LiveData, allowing updates to the stored data.
+    fun loadIncoming(): LiveData<MutableList<FilmItemModel>>{
+        val listData = MutableLiveData<MutableList<FilmItemModel>>()
+        val ref = firebaseDatabase.getReference("Upcoming")
+        //Continuously listens for changes in Firebase.
+        ref.addValueEventListener(object: ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val lists = mutableListOf<FilmItemModel>()
+                for(childSnapshot in snapshot.children){
+                    val item = childSnapshot.getValue(FilmItemModel::class.java)
+                    item?.let { lists.add(it) }
+                }
+                listData.value = lists
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        })
+        return listData
+    }
+
+    fun loadItems(): LiveData<MutableList<FilmItemModel>>{
+        val listData = MutableLiveData<MutableList<FilmItemModel>>()
+        val ref = firebaseDatabase.getReference("Items")
+        ref.addValueEventListener(object: ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val lists = mutableListOf<FilmItemModel>()
+                for(childSnapshot in snapshot.children){
+                    val item = childSnapshot.getValue(FilmItemModel::class.java)
+                    item?.let { lists.add(it) }
+                }
+                listData.value = lists
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        })
+        return listData
+    }
+}
